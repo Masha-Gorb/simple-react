7a1881ace03351cecf967a3a50bf37660ea98435
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-03-31 20:56:23 +0300

    hw8 functional part

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,35 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {PeopleType, UserType} from "../HW8";
+
+type ActionType = {type: 'sort', payload: 'up'} | {type: 'sort-down', payload: 'down'} | {type: 'check', payload: number}
+
+export const homeWorkReducer = (state: PeopleType, action: ActionType): PeopleType => { // need to fix any
     switch (action.type) {
         case 'sort': {
+            const newState = [...state].sort((a,b) => {
+                if(a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
             // need to fix
-            return state
+            return newState
+        }
+        case 'sort-down': {
+            // need to fix
+            const newState = [...state]
+            return newState.reverse()
         }
         case 'check': {
             // need to fix
-            return state
+
+            return state.filter(a => a.age >= action.payload)
         }
         default: return state
     }
 }
+
+// export const homeWorkReducerAC = (payload: string) => {
+//     return {
+//         type: 'sort',
+//         payload
+//     } as const
+// }
